2a024d79dae99bb010ec96cddc7c88349fc96d06
Sidney Gijzen <info@puzzlingpanda.nl>
2022-12-31 15:27:51 +0100

    refactor RoadSection page
    - improve flow page component
    - handle errors with generic ErrorPage component
    - fix bug in map: add unique key to GeoJSON layer

--- src/pages/RoadSection/RoadSectionPage.tsx
+++ src/pages/RoadSection/RoadSectionPage.tsx
@@ -1,12 +1,9 @@
-import { Column, Paragraph, Row } from '@amsterdam/asc-ui'
+import { Column, Row } from '@amsterdam/asc-ui'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 
-import {
-  getRoadSection,
-  RoadSectionFeatureCollection,
-} from '../../api/bereikbaarheid/road-elements'
+import { getRoadSection } from '../../api/bereikbaarheid/road-elements'
 import ContentContainer from '../../shared/components/ContentContainer'
 import Header from '../../shared/components/Header'
 import LoadingSpinner from '../../shared/components/LoadingSpinner'
@@ -19,35 +16,25 @@ const StyledColumn = styled(Column)`
   flex-direction: column;
 `
 
-function NoRoadSection(roadSection: RoadSectionFeatureCollection) {
-  return roadSection.features.length === 0
-}
-
-function displayDetails(roadSection: RoadSectionFeatureCollection) {
-  if (NoRoadSection(roadSection)) {
-    return <Paragraph>Wegvak niet gevonden.</Paragraph>
-  }
-
-  return <RoadSectionDetails properties={roadSection.features[0].properties} />
-}
-
-function displayMap(roadSection: RoadSectionFeatureCollection) {
-  if (NoRoadSection(roadSection)) return
-
-  return <RoadSectionMap roadSections={roadSection} />
-}
-
 const RoadSectionPage = () => {
   const { id: roadSectionId } = useParams()
   useDocumentTitle(`Wegvak ${roadSectionId}`)
-  const {
-    data: roadSection,
-    error,
-    isError,
-    isLoading,
-  } = useQuery('roadSection', ({ signal }) =>
-    getRoadSection(roadSectionId, signal)
-  )
+  const roadSection = useQuery({
+    queryKey: ['roadSection', roadSectionId],
+    queryFn: ({ signal }) => getRoadSection(roadSectionId, signal),
+    useErrorBoundary: true,
+  })
+
+  if (roadSection.isLoading) {
+    return <LoadingSpinner />
+  }
+
+  if (!roadSection.data || roadSection.data.features.length === 0) {
+    throw new Response('Not Found', {
+      status: 404,
+      statusText: 'Wegvak niet gevonden.',
+    })
+  }
 
   return (
     <>
@@ -57,13 +44,13 @@ const RoadSectionPage = () => {
         <ContentContainer>
           <Row>
             <StyledColumn span={6}>
-              {isError && error instanceof Error && <div>{error.message}</div>}
-              {isLoading && <LoadingSpinner />}
-              {roadSection && displayDetails(roadSection)}
+              <RoadSectionDetails
+                properties={roadSection.data.features[0].properties}
+              />
             </StyledColumn>
 
             <StyledColumn span={6}>
-              {roadSection && displayMap(roadSection)}
+              <RoadSectionMap roadSection={roadSection.data.features[0]} />
             </StyledColumn>
           </Row>
         </ContentContainer>
