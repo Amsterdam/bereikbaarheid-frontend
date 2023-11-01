f7ff80319e1455a26fd9d0e95ec8d60e37f5659b
Ruben Sibon <r.sibon@amsterdam.nl>
2023-11-01 14:45:31 +0100

    fix: path util func

--- src/shared/utils/path.ts
+++ src/shared/utils/path.ts
@@ -1,8 +1,22 @@
 import { generatePath } from 'react-router'
 import { RouteIds, getPathTo } from 'routes'
 
-function getGeneratedPath(route: RouteIds, params?: {} | undefined) {
-  return generatePath(getPathTo(route), params)
+function getGeneratedPath(
+  route: RouteIds,
+  params?: Record<string, string> | undefined
+) {
+  return generatePath(
+    `${getPathTo(route)}${
+      params
+        ? Object.keys(params).reduce((acc, key) => {
+            console.log(acc, key)
+
+            return acc ? `${acc}/${key}` : `:${key}`
+          }, '')
+        : ''
+    }`,
+    params
+  )
 }
 
 export { getGeneratedPath }
