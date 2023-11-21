1b06f89821c1c32cf30a4a83b9b410be25884183
Ruben Sibon <r.sibon@amsterdam.nl>
2023-11-21 11:44:09 +0100

    Replace dead and old links (#60)

--- src/pages/ProhibitorySigns/components/RvvDetail/index.ts
+++ src/pages/ProhibitorySigns/components/RvvDetail/index.ts
@@ -1,5 +1,4 @@
-export const linkToPermitCheck =
-  'https://www.amsterdam.nl/veelgevraagd/?productid=%7BE4341B52-1FC0-4E17-AB68-2B3AFE15160A%7D'
+export const linkToPermitCheck = 'https://www.amsterdam.nl/ondernemen/vergunningen/wevos/'
 
 export { RvvDetailExplanation } from './Explanation'
 export { RvvDetailToggle } from './Toggle'
