87978149e4dadd17925bf0e878fc147f7d9ece02
Ruben Sibon <r.sibon@amsterdam.nl>
2024-04-16 16:59:56 +0200

    fix(config): :white_check_mark: test suite gets right api root

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  API_ROOT: `${process.env.REACT_APP_API_ROOT ?? '/'}api/v1`,
+  API_ROOT: `${process.env.NODE_ENV !== 'production' ? process.env.REACT_APP_API_ROOT : '/'}api/v1`,
 }
 
 export default config
