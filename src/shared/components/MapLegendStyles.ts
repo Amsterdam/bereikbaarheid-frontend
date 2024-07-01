7b30c1ef89efff75d243bb7762eb6c17caf9906a
ferhany2002 <ferhan.y2002@gmail.com>
2024-07-01 14:57:18 +0200

    revert scrollbar

--- src/shared/components/MapLegendStyles.ts
+++ src/shared/components/MapLegendStyles.ts
@@ -15,13 +15,4 @@ export const LegendItemsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 2px;
-  overflow: scroll;
-  max-height: 170px;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-
-  -webkit-overflow-scrolling: touch;
-  &::-webkit-scrollbar {
-    display: none;
-  }
 `
