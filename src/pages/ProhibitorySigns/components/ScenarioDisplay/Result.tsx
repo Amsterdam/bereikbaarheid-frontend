d1016fb4a275bb25e04ae9b5454b30f49fc08a37
ferhany <f.yildiz2002@gmail.com>
2026-03-24 15:30:18 +0100

    Add fallback value for parseTime

--- src/pages/ProhibitorySigns/components/ScenarioDisplay/Result.tsx
+++ src/pages/ProhibitorySigns/components/ScenarioDisplay/Result.tsx
@@ -124,7 +124,7 @@ const ScenarioDisplayResult = () => {
 
           <Row halign="flex-start" hasMargin={false} valign="center">
             <Column span={6}>
-              <Link href="https://www.amsterdam.nl/verkeer-vervoer/zwaar-verkeer/" target="_blank" variant="inline">
+              <Link href="https://kaart.amsterdam.nl/zone-zwaar-verkeer#52.3276/4.7480/52.4217/5.0762/topo/14233,14234,14235//" target="_blank" variant="inline">
                 Zone zwaar verkeer
               </Link>
             </Column>
