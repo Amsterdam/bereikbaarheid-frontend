1c12ef76a716bc177d425836bed7dc22d3e01a04
ferhany <f.yildiz2002@gmail.com>
2026-03-25 08:53:04 +0100

    Change laad- en lostijden link

--- src/pages/ProhibitorySigns/components/ScenarioDisplay/Result.tsx
+++ src/pages/ProhibitorySigns/components/ScenarioDisplay/Result.tsx
@@ -82,7 +82,7 @@ const ScenarioDisplayResult = () => {
             <Row halign="flex-start" hasMargin={false} valign="center">
               <Column span={6}>
                 <Link
-                  href="https://www.amsterdam.nl/veelgevraagd/?caseid=%7B839DEE65-CD24-4567-84A5-8DF665BC6384%7D"
+                  href="https://bereikbaarheid.amsterdam.nl/laden-lossen"
                   target="_blank"
                   variant="inline"
                 >
